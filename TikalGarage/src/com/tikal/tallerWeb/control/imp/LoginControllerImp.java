267ceec334dc4e55ddb6e459ed9491b25454ce83
Israel <Israel@192.168.1.70>
2016-09-19 00:21:23 -0500

    Update

--- TikalGarage/src/com/tikal/tallerWeb/control/imp/LoginControllerImp.java
+++ TikalGarage/src/com/tikal/tallerWeb/control/imp/LoginControllerImp.java
@@ -39,75 +39,99 @@ import technology.tikal.accounts.model.session.SessionInfo;
  */
 @Controller
 public class LoginControllerImp implements LoginController {
-    private static final Logger LOGGER = Logger.getLogger(LoginControllerImp.class);
-    @Autowired
-    private LoginService loginService;
-    @Autowired
-    private MensajesControl mensajesControl;
+
+	@Override
+	public void start(StartupController parent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void finish() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean login(String usuario, char[] password, String otp) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void cancel() {
+		// TODO Auto-generated method stub
+		
+	}
+//    private static final Logger LOGGER = Logger.getLogger(LoginControllerImp.class);
 //    @Autowired
-//    @Qualifier("accounts-RestTemplateFactory")
-//    private RestTemplateFactory accountsFactory;
+//    private LoginService loginService;
 //    @Autowired
-//    @Qualifier("taller-RestTemplateFactory")
-//    private RestTemplateFactory tallerFactory;
-//    @Autowired
-//    @Qualifier("customer-RestTemplateFactory")
-//    private RestTemplateFactory customerFactory;
-//    @Autowired
-    private CustomRestErrorHandler errorHandler;
-    
-    private StartupController parent;
-    private boolean iniciado;
-    
-    @Override
-    public boolean login(String usuario, char[] password, String otp) {
-        try {
-            AuthenticationRequest request = new AuthenticationRequest();
-            request.setUser(usuario);
-            request.setPassword(new String(password));
-            if (!StringUtils.isEmpty(otp)) {
-                request.setOtp(Integer.valueOf(otp));
-            }
-            SessionInfo info = loginService.login(request);
-//            tallerFactory.setUsername(info.getUser());
-//            tallerFactory.setPassword(info.getToken());
-//            tallerFactory.setErrorHandler(errorHandler);
-//            tallerFactory.init();
+//    private MensajesControl mensajesControl;
+////    @Autowired
+////    @Qualifier("accounts-RestTemplateFactory")
+////    private RestTemplateFactory accountsFactory;
+////    @Autowired
+////    @Qualifier("taller-RestTemplateFactory")
+////    private RestTemplateFactory tallerFactory;
+////    @Autowired
+////    @Qualifier("customer-RestTemplateFactory")
+////    private RestTemplateFactory customerFactory;
+////    @Autowired
+//    private CustomRestErrorHandler errorHandler;
+//    
+//    private StartupController parent;
+//    private boolean iniciado;
+//    
+//    @Override
+//    public boolean login(String usuario, char[] password, String otp) {
+//        try {
+//            AuthenticationRequest request = new AuthenticationRequest();
+//            request.setUser(usuario);
+//            request.setPassword(new String(password));
+//            if (!StringUtils.isEmpty(otp)) {
+//                request.setOtp(Integer.valueOf(otp));
+//            }
+//            SessionInfo info = loginService.login(request);
+////            tallerFactory.setUsername(info.getUser());
+////            tallerFactory.setPassword(info.getToken());
+////            tallerFactory.setErrorHandler(errorHandler);
+////            tallerFactory.init();
+////
+////            customerFactory.setUsername(info.getUser());
+////            customerFactory.setPassword(info.getToken());
+////            customerFactory.setErrorHandler(errorHandler);
+////            customerFactory.init();
+//            if (parent != null) {
+//                parent.afterLogin();
+//            }
+//            iniciado = false;
+//            return true;
+//        } catch(ResourceAccessException ex) {
+//            LoginControllerImp.LOGGER.error("error en login" + ex.getMessage());
+//        } catch (HttpClientErrorException e) {
+//            mensajesControl.reportaError("Usuario o pasword no validos");
+//        }
+//        return false;
+//    }
 //
-//            customerFactory.setUsername(info.getUser());
-//            customerFactory.setPassword(info.getToken());
-//            customerFactory.setErrorHandler(errorHandler);
-//            customerFactory.init();
-            if (parent != null) {
-                parent.afterLogin();
-            }
-            iniciado = false;
-            return true;
-        } catch(ResourceAccessException ex) {
-            LoginControllerImp.LOGGER.error("error en login" + ex.getMessage());
-        } catch (HttpClientErrorException e) {
-            mensajesControl.reportaError("Usuario o pasword no validos");
-        }
-        return false;
-    }
-
-    @Override
-    public synchronized void start(StartupController parent) {
-        if (!iniciado) {
-//            accountsFactory.init();
-            iniciado = true;
-            this.parent = parent;
-        }
-    }
-
-    @Override
-    public void cancel() {
-//        accountsFactory.shutdown();
-        System.exit(0);
-    }
-
-    @Override
-    public void finish() {
-        
-    }
+//    @Override
+//    public synchronized void start(StartupController parent) {
+//        if (!iniciado) {
+////            accountsFactory.init();
+//            iniciado = true;
+//            this.parent = parent;
+//        }
+//    }
+//
+//    @Override
+//    public void cancel() {
+////        accountsFactory.shutdown();
+//        System.exit(0);
+//    }
+//
+//    @Override
+//    public void finish() {
+//        
+//    }
 }
--- TikalGarage/src/com/tikal/tallerWeb/control/imp/MechanicalWebController.java
+++ TikalGarage/src/com/tikal/tallerWeb/control/imp/MechanicalWebController.java
@@ -1,20 +1,65 @@
 package com.tikal.tallerWeb.control.imp;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.repackaged.com.google.gson.Gson;
 
 @Controller
 public class MechanicalWebController {
 
-	@RequestMapping(value={"/prueba"}, method = RequestMethod.GET)
-	public String prueba(HttpServletRequest request, 	HttpServletResponse response) throws IOException{
-//		response.getWriter().println("llega");
-		return "akdsjf";
+	@RequestMapping(value = { "/prueba" }, method = RequestMethod.GET)
+	public void prueba(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getWriter().println("llega");
+		// return "akdsjf";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public void add(HttpServletRequest request,HttpServletResponse response) throws IOException {
+
+		String name = "Israel";
+		String email = "correo@falso.com";
+
+		Key customerKey = KeyFactory.createKey("DatosClienteOS", email);
+
+		Entity customer = new Entity("DatosClienteOS", customerKey);
+		customer.setProperty("name", name);
+		customer.setProperty("email", email);
+
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		datastore.put(customer);
+		response.getWriter().write("Elemento agregado");
+
+	}
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public void listCustomer(HttpServletResponse response) throws IOException {
+
+		DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+		Query query = new Query("DatosClienteOS").addSort("email", Query.SortDirection.DESCENDING);
+		List<Entity> customers = datastore.prepare(query).asList(FetchOptions.Builder.withLimit(10));
+
+		Gson g= new Gson();
+		String b= g.toJson(customers);
+		
+		response.getWriter().print(b);
+
+
 	}
 }
